b8c22831fb03ef40f29ef00de9fefbb1da9ef811
joeabrams026 <joeabrams026@yahoo.com>
2017-02-14 06:21:41 +0000

    generalized astar function

--- day13.py
+++ day13.py
@@ -1,12 +1,4 @@
-from heapq import heappush, heappop
-from math import sqrt
-
-def onesInNSlow(n):
-    mod = n % 2
-    if n/2 == 0:
-        return mod
-    else:
-        return mod + onesInN(n/2)
+from util import astar
         
 def onesInN(n):
     total = 0
@@ -23,48 +15,17 @@ assert onesInN(3) == 2
 def isWall(xy):
     x,y = xy
     fave = 1362
-    #fave=10
     total = (x*x + 3*x + 2*x*y + y + y*y) + fave
     return (onesInN(total) % 2 == 1)
     
-def path(s, prevs):
-    return ([] if (s is None) else path(prevs[s], prevs) + [s])
-    
-def moves(xy):
+def possible_moves(xy):
     x,y = xy
-    return (coord for coord in ((x+1,y),(x-1,y),(x,y+1),(x,y-1)) if coord[0] >= 0 and coord[1] >=0 and not isWall(coord))
+    return (coord for coord in ((x+1,y),(x-1,y),(x,y+1),(x,y-1)) 
+        if coord[0] >= 0 and coord[1] >=0 and not isWall(coord))
 
-def heuristic(xy,goalxy):
+def distance_left(xy,goalxy):
     x1,y1 = xy
     x2,y2 = goalxy
     return abs(x1-x2) + abs(y1-y2)
-
-def solve(goalxy):
-    goalx,goaly = goalxy
-    prevs = {}
-    move_counts = {} # state : moves_to_state
-    q = []
     
-    def add(xy,prev, move_count):
-        if not xy in move_counts or move_count <= move_counts[xy]:
-            x,y = xy
-            heappush(q,(heuristic(xy,goalxy)+move_count, xy, move_count))
-            prevs[xy] = prev
-            move_counts[xy] = move_count
-            
-    add((1,1),None,0)
-    
-    while len(q) > 0:
-        popped = heappop(q)
-        _, xy,move_count = popped
-        x,y = xy
-        if x == goalx and y == goaly:
-            return path(xy,prevs)
-            
-        for move in moves(xy):
-            add(move, xy, move_count+1)
-            
-    
-print len(solve((31,39))) - 1
-#print solve((7,4))
-#print solve((2,2))
+print len(astar((1,1), (31,39), distance_left, possible_moves)) - 1
--- util.py
+++ util.py
@@ -1,25 +1,38 @@
-# Some utils from Norvig http://nbviewer.jupyter.org/url/norvig.com/ipython/Advent%20of%20Code.ipynb
+''' some utils mostly borrowed from Norvig http://nbviewer.jupyter.org/url/norvig.com/ipython/Advent%20of%20Code.ipynb'''
+from heapq import heappush, heappop
 
 def Input(day):
     "Open this day's input file."
     filename = 'day{}.data'.format(day)
     return open(filename)
     
-def astar_search(start, h_func, moves_func):
-    "Find a shortest sequence of states from start to a goal state (a state s with h_func(s) == 0)."
-    frontier  = [(h_func(start), start)] # A priority queue, ordered by path length, f = g + h
-    previous  = {start: None}  # start state has no previous state; other states will
-    path_cost = {start: 0}     # The cost of the best path to a state.
-    while frontier:
-        (f, s) = heappop(frontier)
-        if h_func(s) == 0:
-            return Path(previous, s)
-        for s2 in moves_func(s):
-            new_cost = path_cost[s] + 1
-            if s2 not in path_cost or new_cost < path_cost[s2]:
-                heappush(frontier, (new_cost + h_func(s2), s2))
-                path_cost[s2] = new_cost
-                previous[s2] = s
-    return dict(fail=True, front=len(frontier), prev=len(previous))
+    
+def path(s, prevs):
+    return ([] if (s is None) else path(prevs[s], prevs) + [s])
+    
+def astar(start_state, goal_state, heuristic, new_states):
+
+    prevs = {}
+    move_counts = {} # state : moves_to_state
+    q = []
+    
+    def add(state,prev, move_count):
+        if not state in move_counts or move_count <= move_counts[state]:
+            heappush(q,(heuristic(state,goal_state)+move_count, state, move_count))
+            prevs[state] = prev
+            move_counts[state] = move_count
+            
+    add(start_state,None,0)
+    
+    while len(q) > 0:
+        popped = heappop(q)
+        _, state, move_count = popped
+        
+        if heuristic(state,goal_state) == 0:
+            return path(state,prevs)
+            
+        for move in new_states(state):
+            add(move, state, move_count+1)
+            
     
     
