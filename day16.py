f201d2ec25f2e86aebce0d3874493269c5ce05ee
Joe Abrams <joeabrams026@yahoo.com>
2017-08-22 15:57:00 -0700

    day 16 part 2

--- day16.py
+++ day16.py
@@ -1,49 +1,45 @@
+from bitarray import bitarray
+
 def dragonize(a):
-	b = []
-	for bit in a[::-1]:
-		if bit == '1':
-			b.append('0')
-		else:
-			b.append('1')
-	return a + '0' + ''.join(b)
+	b = a.copy()
+	b.reverse()
+	b.invert()
+
+	a.append(False)
+	a.extend(b)
+	return a
 
 def randomize(a, desired_len):
-	while len(a) < desired_len:
+	while a.length() < desired_len:
 		a = dragonize(a)
 
 	return a[:desired_len]
 
-def even(a):
-	return len(a) % 2
-
 def checksum(a):
 
-	orig = list(a)
+	orig = a
+	chk_arr = bitarray()
 
-	for z in xrange(100):
-		chk_arr = []
+	while chk_arr.length() % 2 == 0:
+		chk_arr = bitarray()
 
 		for idx in xrange(len(orig)/2):
 			i,j = orig[idx*2:idx*2 + 2]
-			if i == j:
-				chk_arr.append('1')
-			else:
-				chk_arr.append('0')
-
-
-		if len(chk_arr) % 2 == 1:
-			return ''.join(chk_arr)
+			chk_arr.append(i == j)
 
 		orig = chk_arr
 
+	return chk_arr
+
 
 print "Tests:"
-print dragonize('1') == '100'
-print dragonize('0') == '001'
-print dragonize('11111') == '11111000000'
-print dragonize('111100001010') == '1111000010100101011110000'
+print dragonize(bitarray('1')) == bitarray('100')
+print dragonize(bitarray('0')) == bitarray('001')
+print dragonize(bitarray('11111')) == bitarray('11111000000')
+print dragonize(bitarray('111100001010')) == bitarray('1111000010100101011110000')
 
-print randomize('10000', 20) == '10000011110010000111'
+print randomize(bitarray('10000'), 20) == bitarray('10000011110010000111')
 
-print checksum(randomize('10000', 20))
-print checksum(randomize('01111010110010011', 35651584))
+print "test 1", checksum(randomize(bitarray('10000'), 20))
+print "part 1", checksum(randomize(bitarray('01111010110010011'), 272))
+print "part 2", checksum(randomize(bitarray('01111010110010011'), 35651584))
